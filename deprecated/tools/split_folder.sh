9b4919247c7470010569ea17f055a7da4aa4bf72
Julian <yg1811@hotmail.com>
2016-06-17 19:21:35 +0800

    remove deprecated

--- deprecated/tools/split_folder.sh
+++
@@ -1,6 +0,0 @@
-i=0; 
-for f in *; 
-	do d=subset_$(printf %03d $((i/100+1))); 
-	mkdir -p $d; mv "$f" $d; 
-	let i++; 
-done
--- deprecated/train/finetune.py
+++
@@ -1,4 +0,0 @@
-from tuner import Tuner
-
-job = Tuner(GPU=1)
-job.manual_tune(2)
